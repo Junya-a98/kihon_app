ca038383373887731ce1bb16db26055bde9305ff
junya <lizglis@icloud.com>
2025-04-29 15:42:57 +0900

    Env refactor: .env化 & duplicate category fix + migrations

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0)=)!y9ca%czv08b!j8x8db!wkkk@eq@083!1o!@_vu7b=33ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
